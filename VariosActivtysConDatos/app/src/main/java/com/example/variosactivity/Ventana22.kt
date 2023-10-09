9450dc91b33d0a9294b495d686fa278daec19e5a
Jose Romero <jose14romero2003@gmail.com>
2023-10-09 18:38:36 +0200

    Nuevo commit bro

--- VariosActivtysConDatos/app/src/main/java/com/example/variosactivity/Ventana22.kt
+++ VariosActivtysConDatos/app/src/main/java/com/example/variosactivity/Ventana22.kt
@@ -1,25 +1,14 @@
 package com.example.variosactivity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.variosactivity.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class Ventana22 : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.boton.setOnClickListener{
-            abrirVentana2()
-        }
-
-
-    }
-
-    private fun abrirVentana2() {
-        var miIntnet:Intent= Intent(this,Ventana22::class.java)
-        startActivity(miIntnet)
     }
 }
